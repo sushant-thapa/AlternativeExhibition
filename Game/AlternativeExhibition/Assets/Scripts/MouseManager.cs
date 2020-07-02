dd5044699206154e0f0ff7961a3142c14044b4bf
Sushant <073bct547.sushant@pcampus.edu.np>
2020-07-02 15:47:03 +0545

    So this version contains camera switching with keys ASDFG

--- Game/AlternativeExhibition/Assets/Scripts/MouseManager.cs
+++ Game/AlternativeExhibition/Assets/Scripts/MouseManager.cs
@@ -9,19 +9,23 @@ public class MouseManager : MonoBehaviour
 {
     public LayerMask clickableLayer;
     public EventVector3 onClickEnvironment; // So this is an instance of an event
+    public Camera currentCamera;
 
-
-    RaycastHit hit;
+    // The following are for cursors
+    
+    //public Texture2D pointer; // This is meant to be used to point at the floor to navigate
+    //public Texture2D target; 	// This will be used to view paintings on the wall
+    private RaycastHit hit;
    private void Update()
    {
-       
-       if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out hit,50,clickableLayer.value))
-       {
-          if(Input.GetMouseButtonDown(0))
-          {
-              onClickEnvironment.Invoke(hit.point);
+      if(Physics.Raycast(currentCamera.ScreenPointToRay(Input.mousePosition), out hit,50, clickableLayer.value))
+      {
+         if (Input.GetMouseButtonDown(0)){
+            onClickEnvironment.Invoke(hit.point); 
           }
-       }
+      }
+
+   
    }
 }
 
--- Game/AlternativeExhibition/Assets/Scripts/SwitchCamera.cs
+++ Game/AlternativeExhibition/Assets/Scripts/SwitchCamera.cs
@@ -4,18 +4,85 @@ using UnityEngine;
 
 public class SwitchCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        print("hello");
-    }
-
+	public Camera PlayerCam;
+    public Camera CameraA;
+    public Camera CameraS;
+    public Camera CameraD;
+    public Camera CameraF;
+    public Camera CameraG;
+    
     // Update is called once per frame
     void Update()
     {
+    		
         if (Input.GetKeyDown("space"))
         {
         	print("space");
+        	PlayerCam.enabled = true;
+        	CameraA.enabled = false;
+        	CameraS.enabled=false;
+        	CameraD.enabled=false;
+        	CameraF.enabled=false;
+        	CameraG.enabled=false;
+    
         }
+        
+         if (Input.GetKeyDown("a"))
+        {
+        	print("a");
+        	PlayerCam.enabled = false;
+			CameraA.enabled=true;
+        	CameraS.enabled=false;
+        	CameraD.enabled=false;
+        	CameraF.enabled=false;
+        	CameraG.enabled=false;
+    
+        }
+        
+         if (Input.GetKeyDown("s"))
+        {
+        	print("s");
+        	PlayerCam.enabled = false;
+        	CameraA.enabled = false;
+        	CameraS.enabled=true;
+        	CameraD.enabled=false;
+        	CameraF.enabled=false;
+        	CameraG.enabled=false;
+    
+        }
+        
+         if (Input.GetKeyDown("d"))
+        {
+        	print("d");
+        	PlayerCam.enabled = false;
+        	CameraA.enabled = false;
+        	CameraS.enabled=false;
+        	CameraD.enabled=true;
+        	CameraF.enabled=false;
+        	CameraG.enabled=false;
+    
+        }
+        
+         if (Input.GetKeyDown("f"))
+        {
+        	PlayerCam.enabled = false;
+        	CameraA.enabled = false;
+        	CameraS.enabled=false;
+        	CameraD.enabled=false;
+        	CameraF.enabled=true;
+        	CameraG.enabled=false;
+    
+        }
+         if (Input.GetKeyDown("g"))
+         {
+         	print("g");
+         	PlayerCam.enabled = false;
+        	CameraA.enabled = false;
+        	CameraS.enabled=false;
+        	CameraD.enabled=false;
+        	CameraF.enabled=false;
+        	CameraG.enabled=true;
+        }
+    
+         }
     }
-}
