dbc42af799e68e67e33ee025dbc7f966bd9ad25c
Sushant <073bct547.sushant@pcampus.edu.np>
2020-07-07 17:47:18 +0545

    This version successfully has a texture of the plane changed from a webpage. But uses a depreciated www. Should migrate to UnityWebRequest

---
+++ Game/AlternativeExhibition/Assets/Scripts/Experiment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Experiment : MonoBehaviour
+{
+    public Material materialToChange;
+    Material m_Material;
+
+  
+
+
+
+    public string url = "https://d279m997dpfwgl.cloudfront.net/wp/2016/09/tree.jpg";
+    IEnumerator Start()
+    {
+        using (WWW www = new WWW(url))
+        {
+            yield return www;
+            Renderer renderer = GetComponent<Renderer>();
+            renderer.material.mainTexture = www.texture;
+        }
+    }
+}
