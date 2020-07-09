010a6dd093ea92a1a07f281b3af6af7bb5065629
Sushant <073bct547.sushant@pcampus.edu.np>
2020-07-09 13:53:43 +0545

    This version has a fully working game where we can load the texture of a gameobject material from a HTTP request. When downloaded is printed on console, we can press spacebar to change the texture

--- Game/AlternativeExhibition/Assets/Scripts/DynamicTextureFromWeb.cs
+++ Game/AlternativeExhibition/Assets/Scripts/DynamicTextureFromWeb.cs
@@ -1,3 +1,4 @@
+ 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,27 +7,58 @@ using UnityEngine.Networking;
 public class DynamicTextureFromWeb : MonoBehaviour
 {
 
-    Material m_Material;
-	private UnityWebRequest UWR;
+    public GameObject[] Painting;
+	private UnityWebRequest[] UWR;
+	private string[] URLs;
+	private bool isDownloadDone;
 	void Start()
 	{
-		m_Material = GetComponent<Renderer>().material;
-		UWR = UnityWebRequestTexture.GetTexture("https://png.pngtree.com/png-clipart/20190612/original/pngtree-the-red-letter-e-png-image_3396515.jpg");
-		UWR.SendWebRequest();
+		URLs = new string[] {"https://pkperfumes.com/wp-content/uploads/2016/05/Starry-Night-Van-Gogh-retouched-medium.jpg",
+						"https://s4.scoopwhoop.com/anj/paintings/778914987.jpg",
+						"https://media.overstockart.com/optimized/cache/data/product_images/VE2140-1000x1000.jpg",
+						"https://manjitkumar.files.wordpress.com/2011/05/feynman-3001.jpg",
+						"https://hexagongallery.com/wp-content/uploads/2017/01/GWB_SIGNED_RED-717x1000.jpg",
+						"http://3.bp.blogspot.com/-wgkAKkeNgXk/T9ky-RubhbI/AAAAAAAAC3E/-F6-6hfFQm0/s1600/The+Beatles+-+Abbey+Road.jpg"};
+
+		UWR = new UnityWebRequest[6];
+
+		for (int i =0;i<URLs.Length;i++){
+			UWR[i] = UnityWebRequestTexture.GetTexture(URLs[i]);
+		}
+
+for (int i =0;i<URLs.Length;i++){
+			UWR[i].SendWebRequest();
+		}
+		
+		
 	
 	}
 
 	void Update()
 	{
+		isDownloadDone = true;
+		for (int i=0;i<6;i++)
+		{
+			isDownloadDone = UWR[i].isDone & isDownloadDone;
+		}
+
+		if (isDownloadDone)
+		{
+			print("downloaded");
+		}
 
 		if(Input.GetKeyDown("space"))
 		{
 			print("hello");
-		if (UWR.isDone)
-		{
-			m_Material.mainTexture = DownloadHandlerTexture.GetContent(UWR);
+
+			for (int i=0;i<6;i++){
+				
+				if (UWR[i].isDone)
+				{
+					Painting[i].GetComponent<Renderer>().material.mainTexture = DownloadHandlerTexture.GetContent(UWR[i]);
+				}	
+			}
 		}
-	}
 	}
 	
 
