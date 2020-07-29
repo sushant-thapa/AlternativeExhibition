f0a3181b32bfb55e043b64cf188a3c113b4add81
Sushant <thapa.sushant.ts@gmail.com>
2020-07-29 10:28:31 +0545

    Here we have one a script that changes the painting on the floor when the image has been downloaded

---
+++ Game/AlternativeExhibition/Assets/Scripts/DownloadTexture.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DownloadTexture : MonoBehaviour
+{
+
+	public GameObject Painting;
+	private string URL;
+	private bool isDownloadDone;
+	private UnityWebRequest UWR;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        URL = "https://explorug.com/v2/Cache/9349E1D53DF5412E4A3961F68EA1313E/Designs/ART%20ACRYLIC/Cabalape.rendered.jpg";
+        UWR = UnityWebRequestTexture.GetTexture(URL);
+        UWR.SendWebRequest();
+
+    }	
+
+    // Update is called once per frame
+    void Update()
+    {
+        	if( UWR.isDone) 
+        		{
+        			print("yes you may press space");
+        			Painting.GetComponent<Renderer>().material.mainTexture = DownloadHandlerTexture.GetContent(UWR);
+        		}
+    }
+}
---
+++ Game/AlternativeExhibition/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MouseLook : MonoBehaviour
+{
+	public float mouseSensitivity;	
+	private float xRotation;
+	public Transform playerBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xRotation=0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    	// RotateXAuto+=0.0001f;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity *Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation,-90f,90f);
+
+         // this is because it makes rotation intuitive
+        // giving a lock on the mouse
+// 
+        
+        playerBody.Rotate(Vector3.up * mouseX); // Vector3.up is a shorthand for Vector3(0,1,0). so we are rotating by (0,mousex,0);
+        transform.localRotation = Quaternion.Euler(xRotation,0f,0f); 
+    }
+}
